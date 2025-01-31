1b6e4f4462a4ba7036f155399244cfd6b01daa43
jcari-dev <582119jc@gmail.com>
2025-01-31 17:23:05 -0500

    added youtube intro video

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
 				<iframe
 						width="768"
 						height="432"
-						src="https://www.youtube.com/embed/VIDEO_ID"
+						src="https://www.youtube.com/embed/mhSR5BY-SuQ"
 						title="YouTube video player"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 						allowFullScreen
